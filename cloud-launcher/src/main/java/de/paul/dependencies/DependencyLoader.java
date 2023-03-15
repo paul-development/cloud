a5fd025b150a1ba0ebf26c6c18e66f7252c7d2eb
paul-development <p.richter12344@gmail.com>
2023-03-15 23:22:11 +0100

    fixed DependencyLoader

--- cloud-launcher/src/main/java/de/paul/dependencies/DependencyLoader.java
+++ cloud-launcher/src/main/java/de/paul/dependencies/DependencyLoader.java
@@ -1,5 +1,14 @@
 package de.paul.dependencies;
 
+import de.paul.CloudLauncher;
+import de.paul.url.UrlDownloader;
+
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,8 +21,35 @@ import java.util.List;
 public class DependencyLoader {
 
     private final List<Dependency> dependencies;
+    private final URLClassLoader classLoader = ((URLClassLoader) ClassLoader.getSystemClassLoader());
 
     public DependencyLoader() {
         this.dependencies = new ArrayList<>();
+    }
+
+
+    public void loadDependency(String groupId, String artifactId, String version) {
+        CloudLauncher.getInstance().getCloudLogger().info("Loadin dependency " + artifactId + "...");
+        dependencies.add(new Dependency(groupId, artifactId, version));
+        if (new File("dependencies", artifactId + "-" + version + ".jar").exists()) {
+            return;
+        }
+        new UrlDownloader("https://repo1.maven.org/maven2/" + groupId.replace(".", "/") + "/" + artifactId + "/" + version + "/" + artifactId + "-" + version + ".jar", new File("dependencies", artifactId + "-" + version + ".jar")).download();
+    }
+
+    public void addDependency(File file) {
+        try {
+            Method method = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
+            method.setAccessible(true);
+
+            method.invoke(classLoader, file.toURI().toURL());
+        } catch (MalformedURLException | InvocationTargetException | NoSuchMethodException |
+                 IllegalAccessException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public List<Dependency> getDependencies() {
+        return dependencies;
     }
 }
